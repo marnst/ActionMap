836150eb9ddd53e2cfa0f38beca172c5615481e1
marnst <marnstt@gmail.com>
2020-08-12 23:14:10 -0700

    [feat] Counties Map

--- app/javascript/packs/state_map_utils.js
+++ app/javascript/packs/state_map_utils.js
@@ -65,8 +65,8 @@ exports.setupEventHandlers = (stateMap) => {
     const clickCallback = (elem) => {
         // const countyFipsCode = elem.attr('data-county-fips-code');
         // window.location.href = `/state/${stateMap.state.symbol}/county/${countyFipsCode}`;
-        window.location.href =
-        `/search?utf8=✓&address=${elem.attr('data-county-name')}&commit=Search`;
+        temp = `/search?utf8=✓&address=${elem.attr('data-county-name')}&commit=Search`
+        window.location.href= temp ;
     };
     mapUtils.handleMapMouseEvents(targets, hoverHtmlProvider, clickCallback);
 };
