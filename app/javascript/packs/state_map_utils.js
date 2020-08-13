d3b2aeed87639cd88ab7c06751e9bcd2d371cbd0
marnst <marnstt@gmail.com>
2020-08-12 21:45:06 -0700

    [feat]Representative Profile Page

--- app/javascript/packs/state_map_utils.js
+++ app/javascript/packs/state_map_utils.js
@@ -65,7 +65,8 @@ exports.setupEventHandlers = (stateMap) => {
     const clickCallback = (elem) => {
         const countyFipsCode = elem.attr('data-county-fips-code');
         window.location.href = `/state/${stateMap.state.symbol}/county/${countyFipsCode}`;
-        // window.location.href = `/search?utf8=✓&address=${elem.attr('data-county-name')}&commit=Search`;
+        // window.location.href =
+        // `/search?utf8=✓&address=${elem.attr('data-county-name')}&commit=Search`;
     };
     mapUtils.handleMapMouseEvents(targets, hoverHtmlProvider, clickCallback);
 };
